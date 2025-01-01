efb5b725ae3467837a920e2f1653536a72d8b76d
Raghavendra-Aprameya <1rn21cs030.aprameya@gmail.com>
2025-01-01 13:34:11 +0530

    Cors error

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-const app = require("./app");
 const env = require("dotenv");
-
 env.config();
-const port = process.env.PORT || 8001;
+console.log("first");
+const app = require("./app");
 
+const port = process.env.PORT || 8001;
+console.log("Second");
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
